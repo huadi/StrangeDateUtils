a125221674f303daf82bca4fd79d492e38a81e0c
HUA Di <huadi223@gmail.com>
2018-01-05 11:03:00 +0800

    Add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='strangedate',
+    packages=['strangedate'],  # this must be the same as the name above
+    version='0.1',
+    description='',
+    author='HUA Di',
+    author_email='huadi223@gmail.com',
+    url='https://github.com/huadi/StrangeDateUtils',  # use the URL to the github repo
+    download_url='https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',  # I'll explain this in a second
+    keywords=['date'],  # arbitrary keywords
+    classifiers=[],
+)
