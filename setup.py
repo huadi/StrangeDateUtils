9b6a152b0f41c15827f4bfb3cb65201c01eb1ec6
HUA Di <huadi223@gmail.com>
2018-01-08 10:02:49 +0800

    update

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='strangedate',
-    packages=['strangedate'],  # this must be the same as the name above
+    name='strangedateutils',
     version='0.1',
-    description='',
-    author='HUA Di',
+    packages=['strangedateutils'],
+    url='https://github.com/huadi/StrangeDateUtils',
+    license='MIT',
+    author='huadi',
     author_email='huadi223@gmail.com',
-    url='https://github.com/huadi/StrangeDateUtils',  # use the URL to the github repo
-    download_url='https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',  # I'll explain this in a second
-    keywords=['date'],  # arbitrary keywords
-    classifiers=[],
+    description="Some date methods have been used in my daily work. It's not common but may help someone."
 )
